8f6f26ade02a2a19d7909d3d47117aea0ed23121
Dominick Pham <dominick.pham@gmail.com>
2012-01-25 22:34:34 -0500

    serve up config.json

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-/*var Pong = require('./lib/pong.js'),
-	GameManager = Pong.GameManager;
-	Game = Pong.Game;
-
-new GameManager({
-	port: process.env.PORT || 8000
-});*/
-
-var GameManager = require('./lib/game-manager.js'),
-	gameManager = new GameManager({
-		"port": process.env.PORT || 8000,
-		"staticDir": __dirname + '/public'
-	});
+var
+GameManager = require('./lib/game-manager.js'),
+gameManager = new GameManager({
+	"port": process.env.PORT || 8000,
+	"staticDir": __dirname + '/public'
+});
 
